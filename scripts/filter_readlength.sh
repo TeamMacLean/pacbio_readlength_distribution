abeb3f92fc8e39f315b33c5ad2e7a537e11f8f7d
tsl-ramkrishna <ram_krishna.shrestha@tsl.ac.uk>
2019-05-29 11:45:01 +0100

    code update, outputfile option added

--- scripts/filter_readlength.sh
+++ scripts/filter_readlength.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 inputfile=$1
-minlength=$2
+outputfile=$1
+minlength=$3
 
 
 inputfilebasename=$(basename $inputfile)
 
 if [[ ${inputfilebasename} == *".fastq" || $1 == *".fq" ]]; then
-    output=$(echo $(echo ${inputfilebasename} | sed 's/.fastq$//')_readlength_filtered.fastq )
-    cat ${inputfile} | awk -v minlength="$minlength" '{if(NR%4==1){seqid=$0;} else if  (NR%4==2){sequence=$0;} else if(NR%4==0){qual=$0; if(length(sequence) >= minlength){print seqid"\n"sequence"\n+\n"qual}}}' >  $output
+    cat ${inputfile} | awk -v minlength="$minlength" '{if(NR%4==1){seqid=$0;} else if  (NR%4==2){sequence=$0;} else if(NR%4==0){qual=$0; if(length(sequence) >= minlength){print seqid"\n"sequence"\n+\n"qual}}}' >  $outputfile
 
 
 elif [[ ${inputfilebasename} == *".fastq.gz" || $1 == *".fq.gz" ]]; then
-    output=$(echo $(echo ${inputfilebasename} | sed 's/.fastq.gz$//')_readlength_filtered.fastq )
-    zcat ${inputfile} | awk -v minlength="$minlength" '{if(NR%4==1){seqid=$0;} else if  (NR%4==2){sequence=$0;} else if(NR%4==0){qual=$0; if(length(sequence) >= minlength){print seqid"\n"sequence"\n+\n"qual}}}' > $output
+    zcat ${inputfile} | awk -v minlength="$minlength" '{if(NR%4==1){seqid=$0;} else if  (NR%4==2){sequence=$0;} else if(NR%4==0){qual=$0; if(length(sequence) >= minlength){print seqid"\n"sequence"\n+\n"qual}}}' > $outputfile
 
 elif [[ ${inputfilebasename} == *".fasta" || ${inputfilebasename} == *".fa" ]]; then
-    output=$(echo $(echo ${inputfilebasename} | sed 's/.fasta$//')_readlength_filtered.fasta )
-    cat ${inputfile} | awk -v minlength="$minlength" '{if(NR%2==1){seqid=$0;} else if  (NR%2==0){sequence=$0; if(length(sequence)>=minlength){ print  seqid"\n"sequence}} }' >  $output
+    cat ${inputfile} | awk -v minlength="$minlength" '{if(NR%2==1){seqid=$0;} else if  (NR%2==0){sequence=$0; if(length(sequence)>=minlength){ print  seqid"\n"sequence}} }' >  $outputfile
 
 elif [[ ${inputfilebasename} == *".fasta.gz" || $1 == *".fa.gz" ]]; then
-    output=$(echo $(echo ${inputfilebasename} | sed 's/.fasta.gz$//')_readlength_filtered.fasta )
-    zcat ${inputfile} | awk -v minlength="$minlength" '{if(NR%2==1){seqid=$0;} else if  (NR%2==0){sequence=$0; if(length(sequence)>=minlength){ print  seqid"\n"sequence}} }' > $output
+    zcat ${inputfile} | awk -v minlength="$minlength" '{if(NR%2==1){seqid=$0;} else if  (NR%2==0){sequence=$0; if(length(sequence)>=minlength){ print  seqid"\n"sequence}} }' > $outputfile
 
 fi
